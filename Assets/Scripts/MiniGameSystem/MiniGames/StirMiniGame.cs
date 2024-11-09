960e29119d39926fded29aad7cf247fe87c71d85
FilipJJJ <Filip.jarlesjo@gmail.com>
2024-11-09 18:17:35 +0100

    Stiring minigame

--- Assets/Scripts/MiniGameSystem/MiniGames/StirMiniGame.cs
+++ Assets/Scripts/MiniGameSystem/MiniGames/StirMiniGame.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,9 +8,68 @@ public class StirMiniGame : MiniGame
 {
     [SerializeField]
     private InputAction quickTimeValue;
+    private Vector2 targetPress = new Vector2(1, 0);
+
+    [SerializeField]
+    List<StirButton> buttons = new List<StirButton>();
+    private int currentIndex = -1;
+
+    [SerializeField]
+    private float _delayBetweenInputs = .3f;
+    private float _timeLastPressed = 0;
+
+    public override void OnStart(PlayerHandController controller, Organ organ)
+    {
+        base.OnStart(controller, organ);
+        quickTimeValue.performed += OnInput;
+        quickTimeValue.Enable();
+        GoToNextTargetPress();
+    }
+
+    private void OnDisable()
+    {
+        quickTimeValue.performed -= OnInput;
+    }
 
     private void Update()
     {
         
     }
+
+    private void OnInput(InputAction.CallbackContext context)
+    {
+        if(Time.time < _timeLastPressed + _delayBetweenInputs)
+            return;
+
+        if (context.ReadValue<Vector2>().x * targetPress.x != 1 && context.ReadValue<Vector2>().y * targetPress.y != 1)
+            return;
+
+        IncreaseProgress(1);
+        _timeLastPressed = Time.time;
+        GoToNextTargetPress();
+    }
+
+
+    private void GoToNextTargetPress()
+    {
+        currentIndex++;
+        currentIndex %= buttons.Count;
+        targetPress = buttons[currentIndex].position;
+
+        for(int i = 0; i < buttons.Count; i++)
+        {
+            if(i == currentIndex)
+                buttons[i].buttonObject.SetActive(true);
+            else
+                buttons[i].buttonObject.SetActive(false);
+        }
+    }
+}
+
+
+[System.Serializable]
+public class StirButton
+{
+    public GameObject buttonObject;
+    public Vector2 position;
 }
