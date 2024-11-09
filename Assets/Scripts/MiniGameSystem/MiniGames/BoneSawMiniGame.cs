8601e416eacc87b46825569cd1b792e3d59b16ee
xXVikanXx <hotdogmannen2@gmail.com>
2024-11-09 15:34:57 +0100

    Update BoneSawMiniGame.cs

--- Assets/Scripts/MiniGameSystem/MiniGames/BoneSawMiniGame.cs
+++ Assets/Scripts/MiniGameSystem/MiniGames/BoneSawMiniGame.cs
@@ -17,6 +17,7 @@ public class BoneSawMiniGame : MiniGame
     private float m_CurrentZValue;
     private float m_LastZValue;
     private float m_Progression;
+    bool min = false;
 
     public override void OnStart(PlayerHandController controller, Organ organ)
     {
@@ -33,27 +34,41 @@ public class BoneSawMiniGame : MiniGame
 
     private void OnMouseDelta(InputAction.CallbackContext ctx)
     {
+
+        if (ctx.ReadValue<Vector2>().y * mouseSpeed < 0 && !min)
+            return;
+        else if (ctx.ReadValue<Vector2>().y * mouseSpeed > 0 && min)
+            return;
+
         m_CurrentZValue += ctx.ReadValue<Vector2>().y * mouseSpeed;
-
-        if(m_CurrentZValue > maxZ)
+        
+        if(m_CurrentZValue > maxZ && min)
+        { 
             m_CurrentZValue = maxZ;
-        else if (m_CurrentZValue < minZ)
+            min = !min;
+        }
+        else if (m_CurrentZValue < minZ && !min)
+        {
             m_CurrentZValue = minZ;
+            min = !min;
+        }
 
-        Debug.Log(m_CurrentZValue);
+        Debug.Log(Mathf.Abs(m_LastZValue - m_CurrentZValue) * Time.deltaTime);
+
+
 
         if(m_CurrentZValue < maxZ && m_CurrentZValue > minZ)
         {
             Debug.Log("Adding!");
-            m_Progression += Mathf.Abs(m_CurrentZValue - m_LastZValue * Time.deltaTime * 0.001f);
+            m_Progression += 0.5f * Time.deltaTime;
             progressBar.fillAmount = m_Progression;
 
             if(m_Progression >= 1)
             {
-                Win();
+                //Win();
             }
+            m_LastZValue = m_CurrentZValue;
         }
 
-        m_LastZValue = m_CurrentZValue;
     }
 }
